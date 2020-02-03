92be2f849357a323d6b3faddc6dbe116b6e44310
Shashank Mucheli <smucheli@splunk.com>
2020-02-03 11:48:01 -0800

    Two-fer submitted solution

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -7,9 +7,9 @@ import "fmt"
 func ShareWith(name string) string {
 	var response string
 	if len(name) > 0 {
-		response := fmt.Sprintf("One for %s, one for me.", name)
+		response = fmt.Sprintf("One for %s, one for me.", name)
 	} else {
-		response := fmt.Sprintf("One for you, one for me.")
+		response = fmt.Sprintf("One for you, one for me.")
 	}
 	return string(response)
 }
