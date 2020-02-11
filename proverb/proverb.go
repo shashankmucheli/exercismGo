aeae94a453e4dc53dbc8a9738de2d73ab90848a6
Shashank Mucheli <smucheli@splunk.com>
2020-02-10 16:04:48 -0800

    Practice Problem: Proverb

--- proverb/proverb.go
+++ proverb/proverb.go
@@ -1,6 +1,3 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
 // Package proverb should have a package comment that summarizes what it's about.
 // https://golang.org/doc/effective_go.html#commentary
 package proverb
@@ -9,16 +6,12 @@ import (
 	"fmt"
 )
 
-// Proverb should have a comment documenting it.
+// Proverb should have a comment documenting it. Proverb practice problem in exercism.io
 func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	//words := [...]string{"nail", "shoe", "horse", "rider", "message", "battle", "kingdom"}
+
 	s := make([]string, len(rhyme))
 	if len(rhyme) <= 0 {
-		return nil
+		return []string{}
 	}
 	lastLine := fmt.Sprintf("And all for the want of a %s.", rhyme[0])
 	if len(rhyme) == 1 {
@@ -27,9 +20,9 @@ func Proverb(rhyme []string) []string {
 	}
 
 	for i:=0; i<len(rhyme)-1 ;i++  {
-		tmp := fmt.Sprintf("For want of a %s the %s was lost.",rhyme[i],rhyme[i+1])()
-		s = append(s,tmp)
+		tmp := fmt.Sprintf("For want of a %s the %s was lost.",rhyme[i],rhyme[i+1])
+		s[i] = tmp
 	}
-	s = append(s, lastLine)
+	s[len(rhyme)-1] = lastLine
 	return s
 }
